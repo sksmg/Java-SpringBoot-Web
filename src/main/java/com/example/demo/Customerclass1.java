fa43b16ba905d25c3c621a7c3081f757cc77612b
Madhav <madhavameesala950@gmail.com>
2021-10-26 19:21:22 +0530

    comminting files

---
+++ src/main/java/com/example/demo/Customerclass1.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Customerclass1 {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(Customerclass1.class, args);
+	}
+
+}
